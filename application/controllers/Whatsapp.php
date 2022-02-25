416ec19fd26259d2f20b1e126ce10600ceec2bc4
Muhammad Suryono <msuryono@gmail.com>
2022-02-25 10:45:52 +0700

    Fix for message and parameter operation

--- application/controllers/Whatsapp.php
+++ application/controllers/Whatsapp.php
@@ -54,19 +54,24 @@ class Whatsapp extends CI_Controller {
 
 	private function message_transfer(array $data)
 	{
+		$jadwal = explode(" ", $data['jadwal_transfer']);
+		$keteranganPembayaran = $data['project']."/".$data['nomor_stkb']."/".$data['term'];
 		$msg = "Kepada " . $data['penerima'] . ", 
+
 Berikut informasi status pembayaran yang akan Anda terima:
+
 Nama Pembayaran : " . $data['jenis_pembayaran'] . "
 Nomor STKB : ". $data['nomor_stkb'] . "
 Term Pembayaran : ". $data['term'] . "
 Nama Project : ". $data['project'] . "
 No. Rekening Anda : " . $data['nomor_rekening'] . "
-Bank             : " . $data['bank'] . "
-Nama Penerima    : " . $data['pemilik_rekening'] . "
+Bank : " . $data['bank'] . "
+Nama Penerima : " . $data['pemilik_rekening'] . "
 Jumlah Pembayaran : Rp. " . number_format($data['jumlah_sebelumnya'], 0, ',', '.') . "
 Biaya Admin : Rp. " . number_format($data['biaya_trf'], 0, '', '.') . "
 Jumlah Dibayarkan : Rp. " . number_format($data['jumlah'], 0, '', '.') . "
-Status           : Dijadwalkan Pada,  Tanggal : " . $data['jadwal_transfer'] . "
+Status : Dijadwalkan Pada,  Tanggal : " . $jadwal . "
+Keterangan Pembayaran : " . $keteranganPembayaran . "
 
 Jika ada pertanyaan lebih lanjut, silahkan email Divisi Finance ke finance@mri-research-ind.com.
 Hormat kami,
@@ -77,19 +82,24 @@ Finance Marketing Research Indonesia
 
 	public function message_success_transfer(array $data)
 	{
+		$jadwal = explode(" ", $data['jadwal_transfer']);
+		$keteranganPembayaran = $data['project']."/".$data['nomor_stkb']."/".$data['term'];
 		$msg = "Kepada " . $data['penerima'] . ", 
+
 Berikut informasi status pembayaran yang akan Anda terima:
+
 Nama Pembayaran : " . $data['jenis_pembayaran'] . "
 Nomor STKB : ". $data['nomor_stkb'] . "
 Term Pembayaran : ". $data['term'] . "
 Nama Project : ". $data['project'] . "
 No. Rekening Anda : " . $data['nomor_rekening'] . "
-Bank             : " . $data['bank'] . "
-Nama Penerima    : " . $data['pemilik_rekening'] . "
+Bank : " . $data['bank'] . "
+Nama Penerima : " . $data['pemilik_rekening'] . "
 Jumlah : Rp. " . number_format($data['jumlah'] + $data['biaya_transfer'], 0, '', '.') . "
 Biaya Transfer : Rp. " . number_format($data['biaya_transfer'], 0, '', '.') . "
 Jumlah Dibayarkan : Rp. " . number_format($data['jumlah'], 0, '', '.') . "
-Status           : Dibayarkan Pada,  Tanggal : " . $data['jadwal_transfer'] . "
+Status : Dibayarkan Pada,  Tanggal : " . $jadwal[0] . "
+Keterangan Pembayaran : " . $keteranganPembayaran . "
 
 Jika ada pertanyaan lebih lanjut, silahkan email Divisi Finance ke finance@mri-research-ind.com.
 Hormat kami,
